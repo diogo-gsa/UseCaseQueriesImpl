a9bbf3a7c748daef5be5904e45654d048a969e1f
Diogo Anjos <diogo.silva.anjos@tecnico.ulisboa.pt>
2015-10-20 22:34:37 +0100

    Update Q4.sql

--- UseCaseQsImpl/DBMS_Solution/Q4.sql
+++ UseCaseQsImpl/DBMS_Solution/Q4.sql
@@ -1,11 +1,11 @@
-SELECT	device_pk AS device_pk
-		measure_timestamp AS measure_timestamp
-		measure / sum ( measure ) 
-		OVER wintotal *100 AS measure,
-		' Percentage % ' AS measure_unit,
-		' Pie chart of energy consumptions
-		per device ' AS measure_description,
-		device_location AS device_location
+SELECT	device_pk 		AS device_pk
+	measure_timestamp 	AS measure_timestamp
+	measure / sum ( measure ) 
+	OVER wintotal *100 	AS measure,
+	' Percentage % ' 	AS measure_unit,
+	' Pie chart of energy consumptions
+	per device ' 		AS measure_description,
+	device_location 	AS device_location
 FROM 	" Q14_Normalization "
 WHERE 	device_pk <> 0
 	AND index =1

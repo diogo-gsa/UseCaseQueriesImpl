ff31c5623d18194cbe4c633dfb3947a4a396e34f
Diogo Anjos <diogo.silva.anjos@tecnico.ulisboa.pt>
2015-10-20 22:35:39 +0100

    Update Q5.sql

--- UseCaseQsImpl/DBMS_Solution/Q5.sql
+++ UseCaseQsImpl/DBMS_Solution/Q5.sql
@@ -1,12 +1,12 @@
-SELECT 	device_pk 					AS device_pk ,
-		measure_timestamp 			AS measure_timestamp ,
-		rank () OVER sortedwindow 	AS measrure ,
-		measure 					AS current_power_consumption ,
-		' Ranking List Position ' 	AS measure_unit ,
-		' Ranked list of energy consumption
-		per device ' 				AS measure_description ,
-		device_location 			AS measure_location
+SELECT 	device_pk 			AS device_pk ,
+	measure_timestamp 		AS measure_timestamp ,
+	rank () OVER sortedwindow 	AS measrure ,
+	measure 			AS current_power_consumption ,
+	' Ranking List Position ' 	AS measure_unit ,
+	' Ranked list of energy consumption
+	per device ' 			AS measure_description ,
+	device_location 		AS measure_location
 FROM 	" Q14_Normalization "
 WHERE index = 1
-WINDOW sortedwindow AS ( PARTITION BY NULL
-						ORDER BY measure DESC )
+WINDOW sortedwindow AS (PARTITION BY NULL
+			RDER BY measure DESC )
